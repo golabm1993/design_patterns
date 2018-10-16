d2b3084d013a210c4580c72be2baedd18c4b935f
golabm1993 <golabm236@gmail.com>
2018-10-16 17:35:26 +0200

    test

---
+++ factory/src/main/java/pl/gosia/Path.java
@@ -0,0 +1,24 @@
+package pl.gosia;
+
+public class Path {
+
+    public static void findTreasure(Integer wspX, Integer wspY, Integer direction, Integer numberOfStepsY, Integer numberOfStepsX) {
+
+        switch (direction) {
+            case 0:
+                wspY += numberOfStepsY;
+                break;
+            case 1:
+                wspY -= numberOfStepsY;
+                break;
+            case 2:
+                wspX -= numberOfStepsX;
+                break;
+            case 3:
+                wspX += numberOfStepsX;
+                break;
+                default: direction = 0;
+                break;
+        }
+    }
+}
