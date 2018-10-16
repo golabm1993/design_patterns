3e483f7dabddc8f9a77fd3124ed4458edab31ac4
golabm1993 <golabm236@gmail.com>
2018-10-16 13:04:46 +0200

    Empty project for Factory design pattern

---
+++ src/main/java/pl/gosia/App.java
@@ -0,0 +1,13 @@
+package pl.gosia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
